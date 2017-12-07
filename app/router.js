3869255ca8c68f81c77d5d41670b92e2cb0a70e9
sapetti <gsapetti@hotmail.com>
2017-12-08 00:01:45 +0100

    Add process.env.PORT

--- app/router.js
+++ app/router.js
@@ -39,7 +39,9 @@ module.exports = (function() {
       res.send('Got a POST request')
     })
 
-    app.listen(8080, () => console.log('Example app listening on port 8080!'))
+    app.listen(process.env.PORT || 8080, () =>
+      console.log('Example app listening on port 8080!')
+    )
   }
 
   return {
